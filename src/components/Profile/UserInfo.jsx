b1be3bb14abbff0e9b04821fa5c53513e8b7e7f9
alexixx <al3314@yandex.ru>
2023-09-10 22:41:24 +0300

    adaptive version of Profile page is completed

--- src/components/Profile/UserInfo.jsx
+++ src/components/Profile/UserInfo.jsx
@@ -15,14 +15,91 @@ import Button from '../../UI/Button';
 import Tabs from '../../UI/Tabs';
 
 const UserInfo = () => {
+  //   return (
+  //     <>
+  //       <section className="user">
+  //         <div className="user__wallpaper" style={{ backgroundImage: `url(${wallpaperImg})` }}></div>
+  //         <div className="container">
+  //           <div className="user__wrapper">
+  //             <div className="user__column">
+  //               <div className="user__title">Animakid</div>
+  //               <div className="user__info">
+  //                 <div>
+  //                   <span>250k+</span>
+  //                   Volume
+  //                 </div>
+  //                 <div>
+  //                   <span>50+</span>
+  //                   NFTs Sold
+  //                 </div>
+  //                 <div>
+  //                   <span>3000+</span>
+  //                   Followers
+  //                 </div>
+  //               </div>
+  //               <p className="user__subtitle">Bio</p>
+  //               <p className="user__desc">The internet's friendliest designer kid.</p>
+  //               <p className="user__subtitle">Links</p>
+  //               <div className="user__social">
+  //                 <a href="#!">
+  //                   <img src={discordIcon} alt="" />
+  //                 </a>
+  //                 <a href="#!">
+  //                   <img src={youtubeIcon} alt="" />
+  //                 </a>
+  //                 <a href="#!">
+  //                   <img src={twitterIcon} alt="" />
+  //                 </a>
+  //                 <a href="#!">
+  //                   <img src={instagramIcon} alt="" />
+  //                 </a>
+  //               </div>
+  //             </div>
+  //             <div className="user__column">
+  //               <Button value="0xc0E3...B79C" color="purple" icon={copyIcon} />
+  //               <Button value="Follow" icon={plusIcon} />
+  //             </div>
+  //             <img src={authorImg} alt="author" className="user__avatar" />
+  //           </div>
+  //         </div>
+  //       </section>
+  //       <div className="container">
+  //         <Tabs
+  //           values={[
+  //             {
+  //               id: 0,
+  //               title: 'Created',
+  //               counter: 302,
+  //             },
+  //             {
+  //               id: 1,
+  //               title: 'Owned',
+  //               counter: 67,
+  //             },
+  //             {
+  //               id: 2,
+  //               title: 'Collection',
+  //               counter: 4,
+  //             },
+  //           ]}
+  //         />
+  //       </div>
+  //     </>
+  //   );
   return (
     <>
       <section className="user">
         <div className="user__wallpaper" style={{ backgroundImage: `url(${wallpaperImg})` }}></div>
         <div className="container">
           <div className="user__wrapper">
-            <div className="user__column">
+            <div className="user__row">
               <div className="user__title">Animakid</div>
+              <div className="user__button-wrapper">
+                <Button value="0xc0E3...B79C" color="purple" icon={copyIcon} />
+                <Button value="Follow" icon={plusIcon} />
+              </div>
+            </div>
+            <div className="user__row">
               <div className="user__info">
                 <div>
                   <span>250k+</span>
@@ -55,10 +132,6 @@ const UserInfo = () => {
                 </a>
               </div>
             </div>
-            <div className="user__column">
-              <Button value="0xc0E3...B79C" color="purple" icon={copyIcon} />
-              <Button value="Follow" icon={plusIcon} />
-            </div>
             <img src={authorImg} alt="author" className="user__avatar" />
           </div>
         </div>
@@ -82,6 +155,7 @@ const UserInfo = () => {
               counter: 4,
             },
           ]}
+          activeTabDefault="0"
         />
       </div>
     </>
