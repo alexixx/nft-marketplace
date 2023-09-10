1efdcb392948bdf6c5e01a6397fbd2344362c6c9
alexixx <al3314@yandex.ru>
2023-09-10 23:28:38 +0300

    adaptive version of NFT page is completed

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const MobileMenu = () => {
+  return (
+    <div className="mobile-menu">
+      <nav className="mobile-menu__list">
+        <a href="#!" className="mobile-menu__item">
+          Marketplace
+        </a>
+        <a href="#!" className="mobile-menu__item">
+          Rankings
+        </a>
+        <a href="#!" className="mobile-menu__item">
+          Connect a wallet
+        </a>
+        <a href="#!" className="mobile-menu__item">
+          Sign Up
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default MobileMenu;
